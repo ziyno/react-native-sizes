7eb5476d2484899c64fc84edc8011e441bff81e5
Emre Önal <emre@ziyno.com>
2021-11-20 19:34:49 +0300

    Semi removed from all files

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
 declare module 'react-native-sizes' {
-  export function w(pixel: string | number): number;
-  export function h(pixel: string | number): number;
+  export function w(pixel: string | number): number
+  export function h(pixel: string | number): number
 }
--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-import { PixelRatio, useWindowDimensions } from 'react-native';
+import { PixelRatio, useWindowDimensions } from 'react-native'
 
 function h(pixel) {
   if (typeof pixel !== 'number') {
-    pixel = parseFloat(pixel);
+    pixel = parseFloat(pixel)
   }
 
   return PixelRatio.roundToNearestPixel(
     (useWindowDimensions().height * (pixel / 640) * 100) / 100
-  );
+  )
 }
 
 function w(pixel) {
   if (typeof pixel !== 'number') {
-    pixel = parseFloat(pixel);
+    pixel = parseFloat(pixel)
   }
 
   return PixelRatio.roundToNearestPixel(
     (useWindowDimensions().width * (pixel / 360) * 100) / 100
-  );
+  )
 }
 
-export { h, w };
+export { h, w }
